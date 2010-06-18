b055164b2d84d3ca82fb8d4ddb424d4e1822ff50
vbc00297 <vbc00297@CJANKCRK2BMDX.ch2k.honda.ca>
2010-06-18 09:11:16 -0400

    fix TestSecurity.java

--- src/autologin/resources/TestSecurity.java
+++ src/autologin/resources/TestSecurity.java
@@ -1,5 +1,9 @@
 package autologin.resources;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 import com.sun.jersey.core.util.Base64;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
@@ -7,6 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MultivaluedMap;
  
 @Path("/testsecurity")
 @RolesAllowed("admin")
@@ -15,10 +20,10 @@ public class TestSecurity {
     @GET
     @Produces("text/plain")
     @RolesAllowed("user")
-    public String getNumber(@Context HttpHeaders headers){
-        System.out.println( getCredentials(headers));
+    public String printRequestData(@Context HttpHeaders headers){
+        String reply =( dumpData(headers));
  
-        return "1 Secured with User";
+        return reply;
     }
  
      @GET
@@ -26,12 +31,36 @@ public class TestSecurity {
      @RolesAllowed("admin")
      @Produces("text/plain")
     public String getSecureNumber(@Context HttpHeaders headers){
-         System.out.println( getCredentials(headers));
+         System.out.println( dumpData(headers));
  
         return "2 Secured with Admin";
     }
  
-    private String getCredentials(HttpHeaders headers) {
+    private String dumpData(HttpHeaders headers) {
+		  MultivaluedMap <String, String>  m =  headers.getRequestHeaders();
+		  Collection<String>keys = m.keySet();
+		  int i=0;
+		  StringBuffer result = new StringBuffer("");
+		  String msg = "";
+		  for (String key : keys) {
+			  ++i;
+			  
+			  msg = ("header key: "+i+" :"+key + ": -- contents -->");
+			  result.append(msg+"\n");
+			  System.out.println(msg);
+			  List<String> keyvals = headers.getRequestHeader(key);
+			  for (String keyvalstr: keyvals) {
+				  msg = ("  --> "+keyvalstr);
+				  result.append(msg+"\n");
+				  System.out.println(msg);
+			  }
+		  }
+    	/*
+    	i = 0;
+    	for ( String auth : headers.getRequestHeader(HttpHeaders.AUTHORIZATION)) {
+    		++i;
+    		System.out.println(i+" --> "+auth);
+    	}
         String auth = headers.getRequestHeader("authorization").get(0);
  
         auth = auth.substring("Basic ".length());
@@ -40,8 +69,8 @@ public class TestSecurity {
         String password = values[1];
  
         String return_val = "Username = " + username + " Password = "+ password;
- 
-        return return_val;
+ 		*/
+        return result.toString();
     }
  
 }
